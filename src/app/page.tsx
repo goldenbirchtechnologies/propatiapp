9889a0c6902b1faf23a4c47e04c98286d3793ea6
goldenbirchtechnologies <goldenbirchtechnologies@gmail.com>
2026-07-16 20:23:43 +0100

    fix(b1/b2): add use client to homepage client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import MaterialIcon from '@/components/icons/material-icon';
 
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';

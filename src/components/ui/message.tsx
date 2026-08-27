a70e7149cc00924a830ed2407856523a8d6b2ae4
goldenbirchtechnologies <goldenbirchtechnologies@gmail.com>
2026-08-27 17:23:48 +0100

    feat(ui): add BubbleGroup alias and Bubble variant support for message composition

--- src/components/ui/message.tsx
+++ src/components/ui/message.tsx
@@ -1,1 +1,1 @@
-export { Message, MessageAvatar, MessageContent, MessageGroup, MessageHeader, MessageFooter } from './bubble';
+export { Message, MessageAvatar, MessageContent, MessageGroup, MessageHeader, MessageFooter, Bubble, BubbleContent, BubbleGroup } from './bubble';
